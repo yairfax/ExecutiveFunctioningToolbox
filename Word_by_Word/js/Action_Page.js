5ef60a0c4f83ceae39fcb0b7e539c2671b14683e
MajorBerg <besrasal@gmail.com>
2019-02-17 00:47:51 -0500

    Timer implelented
    need to find out how to pause, and how to read post data

--- Word_by_Word/js/Action_Page.js
+++ Word_by_Word/js/Action_Page.js
@@ -1,5 +1,6 @@
-var postdata = "Hello World, lorem ispu dolor sit amet\nHello people I exist. "
+var postdata = "Hello World, lorem ispu dolor sit amet\nHello people I exist."
 var paragraphs = dividewords();
+var paused = true;
 function dividewords()
 {
 	var paragraphs = postdata.split("\n");
@@ -8,4 +9,38 @@ function dividewords()
 		realparagraphs[i] = paragraphs[i].split(" ");
 	};
 	return realparagraphs;
+}
+
+function writewords()
+{
+	writeindivdualword(0,0,1000)
+}
+
+function writeindivdualword(i,q,timeout)
+{	
+	paused = false;
+	document.getElementById("WORDS").innerHTML = paragraphs[i][q];
+	setTimeout(function() {
+		if (q < paragraphs[i].length-2) {
+			writeindivdualword(i,q+1,1000)
+		} else if (q < paragraphs[i].length - 1)
+		{
+			writeindivdualword(i,q+1, 3000)
+		}
+		else if (i < paragraphs.length-1) {
+			writeindivdualword(i + 1, 0, 1000)
+		}
+	}, timeout)
+}
+
+function Pause()
+{
+	if(paused)
+	{
+		//resume
+	}
+	else
+	{
+		//pause
+	}
 }
